5619b36f5088718fb7725276eeb14696d7cf78ef
Deepthii27 <deepthipoojary600@gmail.com>
2025-02-20 18:49:16 +0530

    Add basic HTML structure and JavaScript for API interaction

---
+++ script.js
@@ -0,0 +1,32 @@
+//SELECTORS
+const btnSelector = document.querySelector("#button");
+const inputSelector = document.querySelector("#input");
+const answerSelector = document.querySelector("#answer");
+
+
+//API
+const API_ENDPOINT = "https://yesno.wtf/api";
+
+//FLAGS
+let isRequestInProgress = false;
+
+const setIsRequestInProgress = value => {
+    isRequestInProgress = value;
+};
+
+const setDisableButtonState = isDisabling => {
+    if (isDisabling) {
+        btnSelector.setAttribute("disabled", "disabled");
+    }
+    else {
+        btnSelector.removeAttribute("disabled");
+    }
+};
+
+const cleanupResponse = () => {
+    setTimeout(() => {
+        answerSelector.innerHTML = "";
+        setIsRequestInProgress(false);
+        setDisableButtonState(false);
+    }, 5000);
+};
